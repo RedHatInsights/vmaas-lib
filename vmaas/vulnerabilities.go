a018f4b3a16bbff065928ece97dbe754bbb39c19
Patrik Segedy <psegedy@redhat.com>
2024-08-12 16:02:52 +0200

    feat: remove oval evaluation
    RHINENG-7872

--- vmaas/vulnerabilities.go
+++ vmaas/vulnerabilities.go
@@ -1,8 +1,6 @@
 package vmaas
 
 import (
-	"fmt"
-	"math"
 	"sort"
 	"time"
 
@@ -11,38 +9,11 @@ import (
 	"github.com/redhatinsights/vmaas-lib/vmaas/utils"
 )
 
-const (
-	OvalOperationEvrEquals   = 1
-	OvalOperationEvrLessThan = 2
-
-	OvalCheckExistenceAtLeastOne = 1
-	OvalCheckExistenceNone       = 2
-
-	OvalDefinitionTypePatch         = 1
-	OvalDefinitionTypeVulnerability = 2
-
-	OvalCriteriaOperatorAnd = 1
-	OvalCriteriaOperatorOr  = 2
-)
-
 type VulnerabilitiesCvesDetails struct {
 	Cves          map[string]VulnerabilityDetail
 	ManualCves    map[string]VulnerabilityDetail
 	UnpatchedCves map[string]VulnerabilityDetail
 	LastChange    *time.Time
-}
-
-type ProcessedDefinitions struct {
-	Patch         []*ProcessedDefinition
-	Vulnerability []*ProcessedDefinition
-}
-
-type ProcessedDefinition struct {
-	DefinitionID     DefinitionID
-	DefinitionTypeID int
-	CriteriaID       CriteriaID
-	Packages         []Package
-	Cpe              CpeLabel
 }
 
 type ProductsPackage struct {
@@ -90,7 +61,6 @@ func (r *Request) vulnerabilitiesExtended(c *Cache, opts *options) (*Vulnerabili
 	return &vuln, nil
 }
 
-//nolint:funlen,gocognit
 func evaluate(c *Cache, opts *options, request *Request) (*VulnerabilitiesCvesDetails, error) {
 	cves := VulnerabilitiesCvesDetails{
 		Cves:          make(map[string]VulnerabilityDetail),
@@ -107,10 +77,6 @@ func evaluate(c *Cache, opts *options, request *Request) (*VulnerabilitiesCvesDe
 
 	// get CPEs and ContentSets from repos
 	processed.processRepos(c)
-	definitions, err := processed.processDefinitions(c, opts)
-	if err != nil {
-		return &cves, errors.Wrap(err, "couldn't evaluate OVAL")
-	}
 	products := processed.processProducts(c)
 
 	modules := make(map[string]string)
@@ -119,13 +85,9 @@ func evaluate(c *Cache, opts *options, request *Request) (*VulnerabilitiesCvesDe
 	}
 
 	// 1. evaluate Unpatched CVEs
-	if definitions != nil {
-		for _, definition := range definitions.Vulnerability {
-			cvesOval := c.OvaldefinitionID2Cves[definition.DefinitionID]
-			definition.evaluate(c, modules, cvesOval, &cves, cves.UnpatchedCves)
-		}
+	if opts.evalUnfixed {
+		evaluateUnpatchedCves(c, products, &cves)
 	}
-	evaluateUnpatchedCves(c, products, &cves)
 
 	// 2. evaluate CVEs from Repositories
 	// if CVE is already in Unpatched list -> skip it
@@ -149,25 +111,6 @@ func evaluate(c *Cache, opts *options, request *Request) (*VulnerabilitiesCvesDe
 
 	// 3. evaluate Manually Fixable CVEs
 	// if CVE is already in Unpatched or CVE list -> skip it
-	if definitions != nil {
-		for _, definition := range definitions.Patch {
-			cvesOval := c.OvaldefinitionID2Cves[definition.DefinitionID]
-			// Skip if all CVEs from definition were already found somewhere
-			allCvesFound := true
-			for _, cve := range cvesOval {
-				_, inCves := cves.Cves[cve]
-				_, inManualCves := cves.ManualCves[cve]
-				_, inUnpatchedCves := cves.UnpatchedCves[cve]
-				if !(inCves || inManualCves || inUnpatchedCves) {
-					allCvesFound = false
-				}
-			}
-			if allCvesFound {
-				continue
-			}
-			definition.evaluate(c, modules, cvesOval, &cves, cves.ManualCves)
-		}
-	}
 	evaluateManualCves(c, products, &cves)
 	return &cves, nil
 }
@@ -225,35 +168,6 @@ func evaluateManualCves(c *Cache, products []ProductsPackage, cves *Vulnerabilit
 	}
 }
 
-func (d *ProcessedDefinition) evaluate(
-	c *Cache, modules map[string]string, cvesOval []string, cves *VulnerabilitiesCvesDetails,
-	dst map[string]VulnerabilityDetail,
-) {
-	for _, p := range d.Packages {
-		resultContext := map[string]interface{}{}
-		if evaluateCriteria(c, d.CriteriaID, p.NameID, p.Nevra, modules, resultContext) {
-			for _, cve := range cvesOval {
-				_, inCves := cves.Cves[cve]
-				_, inUnpatchedCves := cves.UnpatchedCves[cve]
-				// Fixable and Unpatched CVEs take precedence over Manually fixable
-				if d.DefinitionTypeID == OvalDefinitionTypePatch && (inCves || inUnpatchedCves) {
-					continue
-				}
-				errataNames := make([]string, 0)
-				for _, erratumID := range c.OvalDefinitionID2ErrataIDs[d.DefinitionID] {
-					errataNames = append(errataNames, c.ErratumID2Name[erratumID])
-				}
-				if data, has := resultContext["module"]; has {
-					module := data.(ModuleStream)
-					updateCves(dst, cve, p, errataNames, d.Cpe, &module)
-				} else {
-					updateCves(dst, cve, p, errataNames, d.Cpe, nil)
-				}
-			}
-		}
-	}
-}
-
 // process repos into CPEs and ContentSets needed for vulnerability evaluation
 func (r *ProcessedRequest) processRepos(c *Cache) {
 	repoIDs, newerReleaseverRepoIDs, contentSetIDs := repos2IDs(c, r.OriginalRequest)
@@ -293,73 +207,6 @@ func (r *ProcessedRequest) processProducts(c *Cache) []ProductsPackage {
 		}
 	}
 	return productsPackages
-}
-
-//nolint:funlen
-func (r *ProcessedRequest) processDefinitions(c *Cache, opts *options) (*ProcessedDefinitions, error) {
-	if r.OriginalRequest.UseCsaf {
-		return nil, nil
-	}
-	candidateDefinitions := repos2definitions(c, r)
-	patchDefinitions := make(map[DefinitionID]*ProcessedDefinition)
-	vulnerabilityDefinitions := make(map[DefinitionID]*ProcessedDefinition)
-	definitions := ProcessedDefinitions{
-		make([]*ProcessedDefinition, 0),
-		make([]*ProcessedDefinition, 0),
-	}
-
-	for _, parsedNevra := range r.Packages {
-		pkgNameID := c.Packagename2ID[parsedNevra.Nevra.Name]
-		allDefinitionsIDs := c.PackagenameID2definitionIDs[pkgNameID]
-		for _, defID := range allDefinitionsIDs {
-			if cpe, ok := candidateDefinitions[defID]; ok {
-				definition := c.OvaldefinitionDetail[defID]
-				switch definition.DefinitionTypeID {
-				case OvalDefinitionTypePatch:
-					processedDefinition, ok := patchDefinitions[defID]
-					if !ok {
-						processedDefinition = &ProcessedDefinition{
-							DefinitionID:     definition.ID,
-							DefinitionTypeID: definition.DefinitionTypeID,
-							CriteriaID:       definition.CriteriaID,
-							// store CPE only for Vulnerability type, field omitted intentionally
-						}
-						patchDefinitions[defID] = processedDefinition
-						definitions.Patch = append(definitions.Patch, processedDefinition)
-					}
-					processedDefinition.Packages = append(processedDefinition.Packages, Package{
-						Nevra:  parsedNevra.Nevra,
-						NameID: pkgNameID,
-						String: parsedNevra.Pkg,
-					})
-				case OvalDefinitionTypeVulnerability:
-					// Skip if unfixed CVE feature flag is disabled
-					if !opts.evalUnfixed {
-						continue
-					}
-					processedDefinition, ok := vulnerabilityDefinitions[defID]
-					if !ok {
-						processedDefinition = &ProcessedDefinition{
-							DefinitionID:     definition.ID,
-							DefinitionTypeID: definition.DefinitionTypeID,
-							CriteriaID:       definition.CriteriaID,
-							Cpe:              c.CpeID2Label[cpe],
-						}
-						vulnerabilityDefinitions[defID] = processedDefinition
-						definitions.Vulnerability = append(definitions.Vulnerability, processedDefinition)
-					}
-					processedDefinition.Packages = append(processedDefinition.Packages, Package{
-						Nevra:  parsedNevra.Nevra,
-						NameID: pkgNameID,
-						String: parsedNevra.Pkg,
-					})
-				default:
-					return nil, fmt.Errorf("unsupported definition type: %d", definition.DefinitionTypeID)
-				}
-			}
-		}
-	}
-	return &definitions, nil
 }
 
 //nolint:gocognit
@@ -492,196 +339,6 @@ func cpes2products(c *Cache, cpes []CpeID, nameID NameID, modules []ModuleStream
 		Package:         Package{Nevra: pkg.Nevra, NameID: nameID, String: pkg.Pkg},
 	}
 	return pp
-}
-
-//nolint:gocognit,nolintlint,funlen
-func repos2definitions(c *Cache, r *ProcessedRequest) map[DefinitionID]CpeID {
-	candidateDefinitions := make(map[DefinitionID]CpeID)
-	// Check CPE-Repo mapping first
-	// Existing CPE-Repo mapping means the repository is eus/aus/e4s
-	// Check exactly matched repositories (current enabled eus/aus/e4s version)
-	// Use all definitions from these, and save vulnerabilities addressed by these definitions
-	cvesFixedCurrentReleasever := make(map[string]bool)
-	for _, cpe := range r.Cpes {
-		if defs, has := c.CpeID2OvalDefinitionIDs[cpe]; has {
-			for _, def := range defs {
-				if cves, has := c.OvaldefinitionID2Cves[def]; has {
-					for _, cve := range cves {
-						cvesFixedCurrentReleasever[cve] = true
-					}
-				}
-				if _, has := candidateDefinitions[def]; !has {
-					candidateDefinitions[def] = cpe
-				}
-			}
-		}
-	}
-	// Check repositories with newer releasever
-	// Consider only definitions addressing CVEs which were not backported to the current stream
-	for _, cpe := range r.NewerReleaseverCpes {
-		if defs, has := c.CpeID2OvalDefinitionIDs[cpe]; has {
-			for _, def := range defs {
-				includeDefinition := true
-				if cves, has := c.OvaldefinitionID2Cves[def]; has {
-					for _, cve := range cves {
-						// Don't add definition to evaluated definitions if the same CVE was
-						// addressed by definition for the current stream
-						if _, has := cvesFixedCurrentReleasever[cve]; has {
-							includeDefinition = false
-							break
-						}
-					}
-				}
-				if includeDefinition {
-					if _, has := candidateDefinitions[def]; !has {
-						candidateDefinitions[def] = cpe
-					}
-				}
-			}
-		}
-	}
-
-	// Not an eus/aus/e4s repo? Use only CPE-CS mapping
-	if len(candidateDefinitions) == 0 {
-		for _, csID := range r.ContentSets {
-			if cpes, has := c.ContentSetID2CpeIDs[csID]; has {
-				for _, cpe := range cpes {
-					if defs, has := c.CpeID2OvalDefinitionIDs[cpe]; has {
-						for _, def := range defs {
-							if _, has := candidateDefinitions[def]; !has {
-								candidateDefinitions[def] = cpe
-							}
-						}
-					}
-				}
-			}
-		}
-	}
-
-	return candidateDefinitions
-}
-
-func evaluateCriteria(c *Cache, criteriaID CriteriaID, pkgNameID NameID, nevra utils.Nevra,
-	modules map[string]string, resultContext map[string]interface{},
-) bool {
-	moduleTestDeps := c.OvalCriteriaID2DepModuleTestIDs[criteriaID]
-	testDeps := c.OvalCriteriaID2DepTestIDs[criteriaID]
-	criteriaDeps := c.OvalCriteriaID2DepCriteriaIDs[criteriaID]
-
-	criteriaType := c.OvalCriteriaID2Type[criteriaID]
-	mustMatch := false
-	requiredMatches := 0
-	switch criteriaType {
-	case OvalCriteriaOperatorAnd:
-		requiredMatches = len(moduleTestDeps) + len(testDeps) + len(criteriaDeps)
-		mustMatch = true
-	case OvalCriteriaOperatorOr:
-		requiredMatches = int(math.Min(1, float64((len(moduleTestDeps) + len(testDeps) + len(criteriaDeps)))))
-		mustMatch = false
-	default:
-		utils.LogError("operator", criteriaType, "Unsupported operator")
-		return false
-	}
-	matches := 0
-
-	for _, m := range moduleTestDeps {
-		if matches >= requiredMatches {
-			break
-		}
-		if evaluateModuleTest(c, m, modules) {
-			matches++
-			resultContext["module"] = c.OvalModuleTestDetail[m].ModuleStream
-		} else if mustMatch { // AND
-			break
-		}
-	}
-
-	for _, t := range testDeps {
-		if matches >= requiredMatches {
-			break
-		}
-		if evaluateTest(c, t, pkgNameID, nevra) {
-			matches++
-		} else if mustMatch { // AND
-			break
-		}
-	}
-
-	for _, cr := range criteriaDeps {
-		if matches >= requiredMatches {
-			break
-		}
-		if evaluateCriteria(c, cr, pkgNameID, nevra, modules, resultContext) {
-			matches++
-		} else if mustMatch { // AND
-			break
-		}
-	}
-
-	return matches >= requiredMatches
-}
-
-func evaluateState(c *Cache, state OvalState, nevra utils.Nevra) (matched bool) {
-	candidateEvr := c.ID2Evr[state.EvrID]
-	switch state.OperationEvr {
-	case OvalOperationEvrEquals:
-		matched = (nevra.Epoch == candidateEvr.Epoch &&
-			nevra.Version == candidateEvr.Version &&
-			nevra.Release == candidateEvr.Release)
-	case OvalOperationEvrLessThan:
-		matched = nevra.NevraCmpEvr(candidateEvr) < 0
-	default:
-		utils.LogError("OvalOperationEvr", state.OperationEvr, "Unsupported OvalOperationEvr")
-		return false
-	}
-
-	candidateArches := c.OvalStateID2Arches[state.ID]
-	if len(candidateArches) > 0 {
-		archID, ok := c.Arch2ID[nevra.Arch]
-		if !ok {
-			utils.LogError("arch", nevra.Arch, "Invalid arch name")
-			return false
-		}
-		if matched {
-			for _, a := range candidateArches {
-				if a == archID {
-					return true
-				}
-			}
-			return false
-		}
-	}
-	return matched
-}
-
-func evaluateModuleTest(c *Cache, moduleTestID ModuleTestID, modules map[string]string) bool {
-	testDetail := c.OvalModuleTestDetail[moduleTestID]
-	return modules[testDetail.ModuleStream.Module] == testDetail.ModuleStream.Stream
-}
-
-func evaluateTest(c *Cache, testID TestID, pkgNameID NameID, nevra utils.Nevra) (matched bool) {
-	candidate := c.OvalTestDetail[testID]
-	pkgNameMatched := pkgNameID == candidate.PkgNameID
-	switch candidate.CheckExistence {
-	case OvalCheckExistenceAtLeastOne:
-		states := c.OvalTestID2States[testID]
-		if pkgNameMatched && len(states) > 0 {
-			for _, s := range states {
-				if evaluateState(c, s, nevra) {
-					matched = true
-					break // at least one
-				}
-			}
-		} else {
-			matched = pkgNameMatched
-		}
-	case OvalCheckExistenceNone:
-		matched = !pkgNameMatched
-	default:
-		utils.LogError("check_existence", candidate.CheckExistence, "Unsupported check_existence")
-		return false
-	}
-	return matched
 }
 
 func updateCves(cves map[string]VulnerabilityDetail, cve string, pkg Package, errata []string, cpe CpeLabel,
