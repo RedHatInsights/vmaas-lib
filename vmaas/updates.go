7ed3e2f2fdff5d0cca1e6b06bb7624a9c7b70845
Patrik Segedy <psegedy@redhat.com>
2024-09-17 14:56:52 +0200

    feat: return only fixable updates from updates receiver

--- vmaas/updates.go
+++ vmaas/updates.go
@@ -11,5 +11,28 @@ func (r *Request) updates(c *Cache, opts *options) (*Updates, error) {
 		return nil, errors.Wrap(err, "couldn't process request")
 	}
 	updates := processed.evaluateRepositories(c, opts)
-	return updates, nil
+	if updates == nil {
+		return updates, nil
+	}
+
+	// filter out manually fixable updates from updates response
+	res := Updates{
+		UpdateList: make(UpdateList, len(updates.UpdateList)),
+		RepoList:   updates.RepoList,
+		RepoPaths:  updates.RepoPaths,
+		ModuleList: updates.ModuleList,
+		Releasever: updates.Releasever,
+		Basearch:   updates.Basearch,
+		LastChange: updates.LastChange,
+	}
+	for pkg, detail := range updates.UpdateList {
+		updates := make([]Update, len(detail.AvailableUpdates))
+		for _, u := range detail.AvailableUpdates {
+			if !u.manuallyFixable {
+				updates = append(updates, u)
+			}
+		}
+		res.UpdateList[pkg] = UpdateDetail{AvailableUpdates: updates}
+	}
+	return &res, nil
 }
