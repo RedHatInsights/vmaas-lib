7bc9cd22d5692db33fc2ce61c04d023a367bf384
Jakub Dugovič <jdugovic@redhat.com>
2025-03-12 18:40:04 +0100

    feat: rewrite vmaas package_name/rpms endpoint in go
    RHINENG-13561

---
+++ vmaas/rpm_pkg_names.go
@@ -0,0 +1,57 @@
+package vmaas
+
+import (
+	"slices"
+
+	"github.com/pkg/errors"
+	"github.com/redhatinsights/vmaas-lib/vmaas/utils"
+)
+
+type ContentData map[string][]string
+
+type RPMPkgNames struct {
+	Names      ContentData `json:"rpm_name_list,omitempty"` // TODO: use `omitzero` from go1.24
+	LastChange string      `json:"last_change,omitempty"`
+}
+
+func (c *Cache) getContentData(rpmNames []string, contentSets []string) ContentData {
+	csMap := make(map[string]bool, len(contentSets))
+	for _, cs := range contentSets {
+		csMap[cs] = true
+	}
+
+	isDuplicate := make(map[string]bool, len(rpmNames))
+	contentData := make(ContentData, len(rpmNames))
+	for _, name := range rpmNames {
+		nameID, found := c.Packagename2ID[name]
+		if !found || isDuplicate[name] {
+			continue
+		}
+
+		csIDs := c.nameID2ContentSetIDs(nameID)
+		labels := c.contentSetIDs2Labels(csIDs)
+		if len(contentSets) != 0 {
+			labels = utils.Intersection(labels, csMap)
+		}
+		slices.Sort(labels)
+		contentData[name] = labels
+		isDuplicate[name] = true
+	}
+	return contentData
+}
+
+func (req *RPMPkgNamesRequest) rpmPkgNames(c *Cache) (*RPMPkgNames, error) { // TODO: implement opts
+	if req.RPMNames == nil {
+		return &RPMPkgNames{}, errors.Wrap(ErrProcessingInput, "'rpm_name_list' is a required property")
+	}
+
+	if len(req.RPMNames) == 0 {
+		return &RPMPkgNames{}, nil
+	}
+
+	res := RPMPkgNames{
+		Names:      c.getContentData(req.RPMNames, req.ContentSets),
+		LastChange: c.DBChange.LastChange,
+	}
+	return &res, nil
+}
