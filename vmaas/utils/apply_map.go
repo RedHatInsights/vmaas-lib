e0af9d4dfc79bb1aa7c6cb88eb62b3e91e19aad9
Jakub Dugovič <jdugovic@redhat.com>
2025-03-12 18:18:10 +0100

    feat: add ApplyMap utility

---
+++ vmaas/utils/apply_map.go
@@ -0,0 +1,9 @@
+package utils
+
+func ApplyMap[K comparable, V any](src []K, mapping map[K]V) []V {
+	dst := make([]V, 0, len(src))
+	for _, x := range src {
+		dst = append(dst, mapping[x])
+	}
+	return dst
+}
