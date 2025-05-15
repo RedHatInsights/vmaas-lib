b85d48b684dc7be76aaebce3c62e7aa75c8bd278
Patrik Segedy <psegedy@redhat.com>
2025-05-15 21:04:51 +0200

    feat: load release graphs
    - ReleaseGraph tree structure allow O(1) lookup by product variant suffix
    - ReleaseNode keeps pointers to Parent node and Children nodes for tree
      navigation
    - GetAncestors returns all ancestors of a given ReleaseNode, it will be
      used to find if a CVE is fixed in previous product variants
    
    RHINENG-18017

---
+++ vmaas/releasegraph.go
@@ -0,0 +1,91 @@
+package vmaas
+
+import (
+	"strings"
+
+	"github.com/pkg/errors"
+)
+
+type ReleaseNode struct {
+	VariantSuffix string
+	Type          string
+	CPEs          []string
+	Children      []*ReleaseNode
+	Parent        *ReleaseNode
+}
+
+type ReleaseGraph struct {
+	GetByVariant map[string]*ReleaseNode
+}
+
+type ReleseGraphRaw struct {
+	Nodes map[string]struct {
+		Type string   `json:"type"`
+		CPEs []string `json:"cpes"`
+	} `json:"nodes"`
+	Edges map[string][]string `json:"edges"`
+}
+
+// Get suffix from release name mappable to product variant suffix
+func getVariantSuffix(variant string) (string, error) {
+	// we are interested only in rhel release name suffix
+	// that we can directly map to rhel product variant
+	// and replace "+" with "." because product variants don't contain "+"
+	// example:
+	// 	release name:     RHEL-9.2.0.Z.MAIN+EUS
+	//  product variant:  Appstream-9.2.0.Z.MAIN.EUS
+	splitted := strings.SplitN(variant, "-", 2)
+	if len(splitted) != 2 {
+		return "", errors.New("release name without '-'")
+	}
+	return strings.ReplaceAll(splitted[1], "+", "."), nil
+}
+
+// Build the ReleaseGraph tree structure
+func (raw *ReleseGraphRaw) BuildGraph() *ReleaseGraph {
+	g := &ReleaseGraph{GetByVariant: make(map[string]*ReleaseNode)}
+
+	// Create all nodes
+	for id, data := range raw.Nodes {
+		variantSuffix, err := getVariantSuffix(id)
+		if err != nil {
+			continue
+		}
+		g.GetByVariant[variantSuffix] = &ReleaseNode{
+			VariantSuffix: variantSuffix,
+			Type:          data.Type,
+			CPEs:          data.CPEs,
+		}
+	}
+
+	// Link parent/children based on edges
+	for fromID, toIDs := range raw.Edges {
+		fromVariant, err := getVariantSuffix(fromID)
+		if err != nil {
+			continue
+		}
+		parent := g.GetByVariant[fromVariant]
+		for _, toID := range toIDs {
+			toVariant, err := getVariantSuffix(toID)
+			if err != nil {
+				continue
+			}
+			child := g.GetByVariant[toVariant]
+			parent.Children = append(parent.Children, child)
+			child.Parent = parent
+		}
+	}
+
+	return g
+}
+
+// Get all parent nodes (ancestors) of a node by a node id - product variant suffix
+func (g *ReleaseGraph) GetAncestors(variant string) []*ReleaseNode {
+	var ancestors []*ReleaseNode
+	node := g.GetByVariant[variant]
+	for node != nil && node.Parent != nil {
+		ancestors = append(ancestors, node.Parent)
+		node = node.Parent
+	}
+	return ancestors
+}
