f80e5c6a867bb54697fc4c61b964d3bd923075d8
Patrik Segedy <psegedy@redhat.com>
2025-05-22 15:50:58 +0200

    chore: update tests for release graph and csaf

--- vmaas/releasegraph_test.go
+++ vmaas/releasegraph_test.go
@@ -21,23 +21,28 @@ const testJSON = `{
 }`
 
 func TestBuildGraphAndAncestors(t *testing.T) {
+	c := &Cache{
+		CpeID2Label: map[CpeID]CpeLabel{1: "cpe:/a:redhat:enterprise_linux:8::appstream"},
+		CpeLabel2ID: map[CpeLabel]CpeID{"cpe:/a:redhat:enterprise_linux:8::appstream": 1},
+	}
+
 	var raw ReleaseGraphRaw
 	err := json.Unmarshal([]byte(testJSON), &raw)
 	require.NoError(t, err)
 
-	graph := raw.BuildGraph()
+	graph := raw.BuildGraph(c.CpeID2Label, c.CpeLabel2ID)
 
 	require.Len(t, graph.GetByVariant, 3)
 
 	node := graph.GetByVariant["8.0.0.Z"]
 	require.NotNil(t, node)
 
-	assert.Equal(t, "8.0.0", node.Parent.VariantSuffix)
+	assert.Equal(t, VariantSuffix("8.0.0"), node.Parent.VariantSuffix)
 	assert.Len(t, node.Children, 1)
-	assert.Equal(t, "8.0.1", node.Children[0].VariantSuffix)
+	assert.Equal(t, VariantSuffix("8.0.1"), node.Children[0].VariantSuffix)
 
 	ancestors := graph.GetAncestors("8.0.1")
 	require.Len(t, ancestors, 2)
-	assert.Equal(t, "8.0.0.Z", ancestors[0].VariantSuffix)
-	assert.Equal(t, "8.0.0", ancestors[1].VariantSuffix)
+	assert.Equal(t, VariantSuffix("8.0.0.Z"), ancestors[0].VariantSuffix)
+	assert.Equal(t, VariantSuffix("8.0.0"), ancestors[1].VariantSuffix)
 }
